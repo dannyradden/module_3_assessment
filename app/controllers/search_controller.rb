738391a49d16ed7ebad5ca98ea0fa602e32b2bcb
dannyradden <dradden@gmail.com>
2017-06-06 15:07:30 -0600

    Start to build search index view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,5 +2,6 @@ class SearchController < ApplicationController
   def index
     service = BestBuyService.new
     @stores = service.stores(params[:zip_code])
+    require "pry"; binding.pry
   end
 end
--- spec/features/user_can_find_stores_spec.rb
+++ spec/features/user_can_find_stores_spec.rb
@@ -2,25 +2,24 @@ require 'rails_helper'
 
 describe 'Dashboard' do
   it 'User can find nearest BestBuy stores' do
-    visit root_path
+    VCR.use_cassette("features/find_stores") do
+      visit root_path
 
-    fill_in :zip_code, with: '80202'
-    click_on 'search'
+      fill_in :zip_code, with: '80202'
+      click_on 'search'
 
-    expect(current_path).to eq("/search")
+      expect(current_path).to eq("/search")
 
-    expect(page).to have_content '16 Total Stores'
-    expect(find('.stores')).to have_selector('div', count: 10)
+      expect(page).to have_content '16 Total Stores'
+      expect(find('.stores')).to have_selector('div', count: 10)
 
-    within('#store1') do
-      expect(page).to have_content 'Name: CHERRY CREEK MALL'
-      expect(page).to have_content 'City: DENVER'
-      expect(page).to have_content 'Distance: 3.45 Miles'
-      expect(page).to have_content 'Phone Number: 303-270-9189'
-      expect(page).to have_content 'Store Type: Mobile SAS'
+      within('#store1') do
+        expect(page).to have_content 'Name: CHERRY CREEK MALL'
+        expect(page).to have_content 'City: DENVER'
+        expect(page).to have_content 'Distance: 3.45 Miles'
+        expect(page).to have_content 'Phone Number: 303-270-9189'
+        expect(page).to have_content 'Store Type: Mobile SAS'
+      end
     end
-
-# https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,distance,phone&pageSize=10&apiKey=tq72hpvtbbd4mue6f23kmsw2
-
   end
 end
