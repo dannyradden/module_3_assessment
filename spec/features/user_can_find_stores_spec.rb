aa0f854036c312b6462b6c96a69c705d2505e1a3
dannyradden <dradden@gmail.com>
2017-06-06 14:23:32 -0600

    Start building out functionality for store search
    Finish spec
    Build search route
    Initialize search controller
    Add search field to items#index

--- spec/features/user_can_find_stores_spec.rb
+++ spec/features/user_can_find_stores_spec.rb
@@ -4,20 +4,23 @@ describe 'Dashboard' do
   it 'User can find nearest BestBuy stores' do
     visit root_path
 
-    fill_in 'Zip Code', with: '80203'
+    fill_in :zip_code, with: '80202'
     click_on 'search'
 
-    expect(page).to have_current_path("/search")
+    expect(current_path).to eq("/search")
 
     expect(page).to have_content '16 Total Stores'
+    expect(find('.stores')).to have_selector('div', count: 10)
 
-    expect(page).to have_content 'Name: CHERRY CREEK MALL'
+    within('#store1') do
+      expect(page).to have_content 'Name: CHERRY CREEK MALL'
+      expect(page).to have_content 'City: DENVER'
+      expect(page).to have_content 'Distance: 3.45 Miles'
+      expect(page).to have_content 'Phone Number: 303-270-9189'
+      expect(page).to have_content 'Store Type: Mobile SAS'
+    end
 
-    # https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name&pageSize=10&apiKey=tq72hpvtbbd4mue6f23kmsw2
+# https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,city,distance,phone&pageSize=10&apiKey=tq72hpvtbbd4mue6f23kmsw2
 
-    # And I should see stores within 25 miles of 80202
-    # And I should see a message that says "16 Total Stores"
-    # And I should see exactly 10 results
-    # And I should see the long name, city, distance, phone number and store type for each of the 10 results
   end
 end
