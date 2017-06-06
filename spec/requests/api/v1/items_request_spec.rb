5acae10167d7dfabaef564cd3ca979d72466aa54
dannyradden <dradden@gmail.com>
2017-06-06 13:37:58 -0600

    Add api endpoints for Show and Destroy items
    Make spec, routes, and controller actions

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -8,7 +8,7 @@ describe "Items API" do
 
     get '/api/v1/items'
 
-    expect(response).to be_success
+    expect(response.status).to eq(200)
 
     items = JSON.parse(response.body)
 
@@ -20,5 +20,54 @@ describe "Items API" do
     expect(items.first).to have_key("image_url")
     expect(items.first).to_not have_key("created_at")
     expect(items.first).to_not have_key("updated_at")
+  end
+
+  it "sends a single item" do
+    3.times do |n|
+      Item.create!(name: "item#{n}", description: "description#{n}", image_url: "url#{n}")
+    end
+
+    get '/api/v1/items/1'
+
+    expect(response.status).to eq(200)
+
+    item = JSON.parse(response.body)
+
+    expect(item).to have_key("id")
+    expect(item).to have_key("name")
+    expect(item["name"]).to eq("item0")
+    expect(item).to have_key("description")
+    expect(item).to have_key("image_url")
+    expect(item).to_not have_key("created_at")
+    expect(item).to_not have_key("updated_at")
+  end
+
+  it "can delete an item" do
+    3.times do |n|
+      Item.create!(name: "item#{n}", description: "description#{n}", image_url: "url#{n}")
+    end
+
+    delete '/api/v1/items/1'
+
+    expect(response.status).to eq(204)
+    expect(Item.count).to eq(2)
+  end
+
+  it "creates an item" do
+    item_params = { name: "Cup", description: "Drink Me", image_url: 'www.cupimage.com' }
+    # 
+    # post '/api/v1/items?name='
+    #
+    # expect(response.status).to eq(200)
+    #
+    # item = JSON.parse(response.body)
+    #
+    # expect(item).to have_key("id")
+    # expect(item).to have_key("name")
+    # expect(item["name"]).to eq("item0")
+    # expect(item).to have_key("description")
+    # expect(item).to have_key("image_url")
+    # expect(item).to_not have_key("created_at")
+    # expect(item).to_not have_key("updated_at")
   end
 end
